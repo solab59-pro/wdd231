54e4b578256920ea21cf3486775caaae7461385a
solab59-pro <jaywisefreeworld59@gmail.com>
2025-08-08 16:40:17 +0100

    discover page

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // ✅ Use relative path so it works on any device or environment
-  fetch('https://solab59-pro.github.io/wdd231/chamber/discover.json')
+  fetch('https://impulsible.github.io/wdd231/chamber/discover.json')
     .then(response => {
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
